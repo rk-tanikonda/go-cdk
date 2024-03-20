9d9c7d35cba4d7c5e659ea3056574fc6c587ec78
rk-akaike <ramakrishna@akaiketech.com>
2024-03-21 03:33:21 +0530

    Add .gitignore, go-cdk_test.go, go.mod, and cdk.json files

--- go-cdk.go
+++ go-cdk.go
@@ -3,6 +3,9 @@ package main
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -24,6 +27,47 @@ func NewGoCdkStack(scope constructs.Construct, id string, props *GoCdkStackProps
 	// queue := awssqs.NewQueue(stack, jsii.String("GoCdkQueue"), &awssqs.QueueProps{
 	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
 	// })
+
+	table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		TableName: jsii.String("userTable"),
+	})
+
+	myFunction := awslambda.NewFunction(stack, jsii.String("myLambdaFunction"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
+		Handler: jsii.String("main"),
+	})
+
+	api := awsapigateway.NewRestApi(stack, jsii.String("myApiGateway"), &awsapigateway.RestApiProps{
+		DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
+			AllowHeaders: jsii.Strings("Content-Type", "Authorization"),
+			AllowMethods: jsii.Strings("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"),
+			AllowOrigins: jsii.Strings("*"),
+		},
+		// DeployOptions: &awsapigateway.StageOptions{
+		// 	LoggingLevel: awsapigateway.MethodLoggingLevel_INFO,
+		// },
+		// EndpointConfiguration: &awsapigateway.EndpointConfiguration{
+		// 	Types: &[]awsapigateway.EndpointType{awsapigateway.EndpointType_REGIONAL},
+		// },
+	})
+
+	integration := awsapigateway.NewLambdaIntegration(myFunction, nil)
+
+	registerResource := api.Root().AddResource(jsii.String("register"), nil)
+	registerResource.AddMethod(jsii.String("POST"), integration, nil)
+
+	loginResource := api.Root().AddResource(jsii.String("login"), nil)
+	loginResource.AddMethod(jsii.String("POST"), integration, nil)
+
+	protectedResource := api.Root().AddResource(jsii.String("protected"), nil)
+	protectedResource.AddMethod(jsii.String("GET"), integration, nil)
+
+	table.GrantReadWriteData(myFunction)
 
 	return stack
 }
